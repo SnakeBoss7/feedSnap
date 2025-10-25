cf842ddc13c469115a604208a236cfc88ba809c8
SnakeBoss7 <rahuldharwal12005@gmail.com>
2025-10-25 16:43:55 +0530

    cors changes

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
- origin: ['http://localhost:3000','http://10.157.84.112:3000'],
+ origin: ['http://localhost:3000','http://10.157.84.112:3000','https://feedsnap-client.onrender.com'],
   credentials: true,
 }));
 app.use(express.json());
