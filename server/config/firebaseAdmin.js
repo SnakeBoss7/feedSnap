798d9f91d524d3b72190e2e22396626c8203beb9
SnakeBoss7 <rahuldharwal12005@gmail.com>
2025-10-25 16:35:47 +0530

    firebase changes

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,6 +1,13 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("../firebase-adminsdk.json");
-
+if(process.env.MODE === "dev")
+  {
+    console.log("env" + process.env.MODE)
+    var serviceAccount = require("../firebase-adminsdk.json");
+  }
+  else
+    {
+      var serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+    }
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
